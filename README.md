70599eab1d540f44959ed8f1189570c04e2dc81f
Marc Lopez <marclopezmarco469@gmail.com>
2024-11-03 19:43:04 +0100

    page estatica

--- README.md
+++
@@ -1,1 +0,0 @@
-<img width="1440" alt="image" src="https://github.com/user-attachments/assets/65fca7f1-3d4c-40c1-9907-3527fea49eba">
--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\PdfController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [PdfController::class, 'index'])->name('index');
